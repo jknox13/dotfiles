eadc42fcfb1758d3a012e2038921b85754a7f406
Joe Knox <jknox@elltel.net>
2022-02-20 15:30:00 +0000

    update: include draft kmonad install in script

--- install.sh
+++ install.sh
@@ -7,3 +7,19 @@ stow nvim
 # check if installed
 # install plugins
 nvim -E +PlugInstall +qall
+
+# ==============    kmonad    ==============
+# download latest binary
+# TODO: add logic to get latest binary
+curl -o /usr/bin/kmonad https://github.com/kmonad/kmonad/releases/tag/0.4.1
+
+# user need uinput permissions
+# groupadd uinput & usermod -aG input $(whoami) & usermod -aG uinput $(whoami)
+# cp system/40-uinput.rules /etc/udev/rules.d
+
+stow kmonad
+
+# enable kmonad service
+# systemctl --user enable kmonad.service
+
+# TODO check keyboard is correct
